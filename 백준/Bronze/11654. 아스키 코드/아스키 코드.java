90c6f741344082b01ef39b0bd895268cf30bb5cc
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-11-24 14:31:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int ask = scanner.next().charAt(0);
+		
+		System.out.println(ask);
+	}
+}
