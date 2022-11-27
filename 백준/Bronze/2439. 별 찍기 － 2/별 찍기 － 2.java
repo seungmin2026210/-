cc77c78a4cdf9c9635c691e6df6caa4c6be6ad2d
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-11-27 20:52:40 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 396 ms, Memory: 20824 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= n; j++) {
+				if (i > j) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
