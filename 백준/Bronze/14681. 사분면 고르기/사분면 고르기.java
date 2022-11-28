379a2d537f473ca8528509091682de2e6fab2b1c
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-11-28 23:10:29 +0900

    [Bronze V] Title: 사분면 고르기, Time: 208 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		if(0<x && 0<y) {
+			System.out.println("1");
+		} else if(0<x && y<0) {
+			System.out.println("4");
+		} else if (x<0 && 0<y) {
+			System.out.println("2");
+		} else {System.out.println("3");
+		}
+	}
+}
