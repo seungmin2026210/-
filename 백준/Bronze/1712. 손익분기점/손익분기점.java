188b415e95896d2f242b05d4eeedcccd4fcd11b6
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-11-30 21:52:19 +0900

    [Bronze II] Title: 손익분기점, Time: 204 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt(); 
+		int c = scanner.nextInt(); 
+		
+		if(c<=b) {
+			System.out.println("-1");
+		} else {System.out.println((a/(c-b))+1);
+		}
+ }
+}
