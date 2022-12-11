27e53f833019fcaba959e8e0eb9dc307672c03a2
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-12-12 08:49:47 +0900

    [Bronze V] Title: N 찍기, Time: 956 ms, Memory: 31332 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int i = scanner.nextInt();
+		
+		for(int j=1; j<=i;j++) {
+			System.out.println(j);
+			}
+		}
+	}
