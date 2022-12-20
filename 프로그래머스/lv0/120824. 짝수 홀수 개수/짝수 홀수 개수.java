9cb7e3e18e60d647489e817a84e16c6f68daf7b5
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-12-20 14:26:01 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+
+        int count1 = 0;
+        int count2 = 0;
+        
+        for(int i=0; i<num_list.length;i++){
+            if(num_list[i]%2==0){
+                count1++;
+            } else count2++;}
+            
+         int[] answer = new int[] {count1,count2};
+            
+        return answer;
+    }
+}
