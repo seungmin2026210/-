76548d64cfe72a113c9934cca2a50bf37b09e87f
seungmin2026210 <118713888+seungmin2026210@users.noreply.github.com>
2022-12-21 10:02:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 1696 ms, Memory: 317988 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
+
 public class Main {
-public static void main(String[] args) {
+	
+	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
+		int[] num= new int[n];
 		int s = 0;
 		int m = 0;
 		
-		for(int i=1; i<=n;i++) {
+		for(int i=0; i<n;i++) {
 			int num1 = scanner.nextInt();
-			if(i==1) {
-				s = num1;
+			num[i] = num1; 
+			
+			if (i==0) {
+				s=num1; 
+				m=num1;  
+			} 
+			
+			if(s>num1) { 
+				s = num1; 
+			}
+			if(m<num1) { 
 				m = num1;
-			} else if(m<=num1) {
-				m = num1;
-			} else if (s>=num1) {
-				s = num1;
 			}
 		}	
-		System.out.println(s +" "+m);
+		System.out.println(s + " "+ m);
 	}
 }
