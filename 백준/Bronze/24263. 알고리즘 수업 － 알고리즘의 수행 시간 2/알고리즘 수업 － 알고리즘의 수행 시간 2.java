303736eedbe111cada4a3e5169cedd6b7e9765be
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-05-23 16:14:28 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(scanner.nextInt());
+		System.out.println("1");
+	}
+}
