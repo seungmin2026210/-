c432478802fc657025854827a417494479b39dd4
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-06-26 21:57:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        long sum =0;
+        
+        for(int i=0; i<3; i++) {
+        	
+        	sum += scanner.nextLong();
+        }
+        
+        System.out.println(sum);
+	}
+}
