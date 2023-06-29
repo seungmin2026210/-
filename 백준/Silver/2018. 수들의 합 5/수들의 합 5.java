eb0631952d4c1cd3612939bc5af5b4c6ed696bf9
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-06-29 21:38:59 +0900

    [Silver V] Title: 수들의 합 5, Time: 292 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,22 +1,27 @@
 import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int answer = 0;
-
-        for(int i=1; i<=n; i++){
-            int sum = 0;
-            for(int j=i; j<=n; j++){
-                sum+=j;
-                if(sum >n) break;
-                else if(sum == n){
-                    answer++;
-                    break;
-                }
-            }
-        }
-        System.out.println(answer);
-    }
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		int answer = 0;
+		
+		for(int i=1; i<=N; i++) {
+			int sum =0;
+			
+			for(int j=i; j<=N; j++) {
+				sum+=j;
+				
+				if(N<sum) {
+					break;
+				} else if (sum==N) {
+					answer++;
+					break;
+				}
+			}
+		}
+		System.out.println(answer);
+	}
 }
