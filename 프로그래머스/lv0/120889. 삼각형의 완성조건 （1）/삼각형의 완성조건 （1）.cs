aedd82aadd9d56c10ef63bf84a847b7c34677c64
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-08-02 20:02:03 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int max =0;
+        int index =0;
+        int sum=0;
+        
+        for(int i=0; i<3; i++){
+            if(max<sides[i]){
+                max = sides[i];
+                index = i;
+            }
+        }
+        
+        for(int i=0; i<3; i++){
+            if(i==index){
+                continue;
+            } else {
+                sum+=sides[i];
+            }
+        }
+        
+        if(max<sum){
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        
+        return answer;
+    }
+}
