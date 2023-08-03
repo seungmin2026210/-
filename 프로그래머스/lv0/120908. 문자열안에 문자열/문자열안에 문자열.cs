2b1be1d8e03643b3688ec1d1149e91715fe66813
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-08-03 22:32:47 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(string str1, string str2) {
+        int answer = 0;
+        
+        if(str1.Contains(str2)==true){
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        return answer;
+    }
+}
