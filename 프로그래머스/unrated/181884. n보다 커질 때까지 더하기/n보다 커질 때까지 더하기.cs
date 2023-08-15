fc737942fa9ffd2f9cf5fcf82cb05bc942e928fb
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-08-15 19:00:35 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        int index = 0;   
+        
+        while (answer<=n){
+            answer += numbers[index];
+            index ++;
+        }
+        
+        return answer;
+    }
+}
