c455d48cd6be92fb72752c4265e7d27b85373eb8
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-08-17 19:42:08 +0900

    [unrated] Title: 부분 문자열, Time: 0.22 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(string str1, string str2) {
+        int answer = 0;
+        
+        if(str2.Contains(str1)==true) answer= 1;
+        
+        
+        return answer;
+    }
+}
