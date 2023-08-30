bcd0821636574db41dfd0cd0f3a93379034fb17b
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-08-30 19:03:35 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.45 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[n/k];
+        int index=0;
+        for(int i=1; i<=n; i++){
+            if(i%k==0) {
+                answer[index] = i;
+                index++;
+            }
+        }
+        return answer;
+    }
+}
