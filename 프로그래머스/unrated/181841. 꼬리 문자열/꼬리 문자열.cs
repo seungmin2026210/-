da33e847a010369431e53b627f46109542fe279a
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-09-01 20:54:05 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.22 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(string[] str_list, string ex) {
+        string answer = "";
+        
+        for(int i=0; i<str_list.Length; i++){
+            
+            if(str_list[i].Contains(ex)==false){
+                answer+=str_list[i];
+            }
+        }
+        return answer;
+    }
+}
