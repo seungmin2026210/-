e6d6d8cf7c43ded88b9aeef58f0c7e7be3b43557
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-09-03 22:12:53 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.22 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if( a%2 != 0 && b%2!= 0 ){
+            answer = (a*a) + (b*b);
+        } else if( a%2 != 0 || b%2!= 0 ){
+            answer = 2*(a+b);
+        } else {
+            answer = Math.Abs(a-b);
+        }
+        return answer;
+    }
+}
