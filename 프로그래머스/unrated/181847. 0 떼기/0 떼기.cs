bdbee251c6558ab2b3ff1554ebc7ea0f92459104
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-09-05 20:14:59 +0900

    [unrated] Title: 0 떼기, Time: 0.78 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public string solution(string n_str) {
+        int a = (Int32.Parse(n_str));
+        string answer = a.ToString();
+        return answer;
+    }
+}
