d9bda27084ef83d60e0fec04541e06209ac6bc5b
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-09-12 19:51:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        
+        while( (6 * answer)%n != 0 ){
+            answer++;            
+        }
+        return answer;
+    }
+}
