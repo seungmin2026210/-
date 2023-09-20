101753e5b86006f7047995667124c1de47a31b5b
이승민 <118713888+seungmin2026210@users.noreply.github.com>
2023-09-20 20:07:28 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.20 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] arr) {
+        int answer = 1;
+        
+        for(int i=0; i<arr.GetLength(0); i++){
+            for(int j=0; j<i; j++){
+                if(arr[i, j]!=arr[j, i]){
+                    answer=0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
